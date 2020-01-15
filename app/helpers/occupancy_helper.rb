bf146250b0a962621b0ff88903a0ee538b3fff80
Shannon <shanwpf@gmail.com>
2020-01-15 16:34:24 +0800

    Return average values as int instead of str

--- app/helpers/occupancy_helper.rb
+++ app/helpers/occupancy_helper.rb
@@ -5,8 +5,10 @@ module OccupancyHelper
         from occupancies
         group by cast(created_at as date), date_part('hour', created_at)
         order by cast(created_at as date), date_part('hour', created_at)"
-    ).entries
-    puts result
+    )
+    result.each do |row|
+      row['avg'] = row['avg'].to_f.round
+    end
     result.to_json
   end
 end
